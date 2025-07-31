7f3980d365a8f55e20c077a3f812a0754c3a1535
JIHEE <jhkimm96@gmail.com>
2025-08-01 01:03:36 +0900

    init:springboot-web-project 초기 업로드

---
+++ src/main/java/dev/springbootweb/SpringbootWebProjectApplication.java
@@ -0,0 +1,13 @@
+package dev.springbootweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootWebProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootWebProjectApplication.class, args);
+    }
+
+}
